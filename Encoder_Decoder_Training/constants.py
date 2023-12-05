96ab114d81b2c4383854e6364cb3b484936e3f78
ChristianMitrache <christianstatsmitrache@gmail.com>
2023-12-04 19:15:17 -0500

    Adding functionality for learning rate scheduler

--- Encoder_Decoder_Training/constants.py
+++ Encoder_Decoder_Training/constants.py
@@ -5,17 +5,16 @@ RAW_MP3_DIR = os.path.join("data", "unprocessed_songs")
 ENCODER_DECODER_PROCESSED_DIR = os.path.join("data", "chunked_songs")
 VAL_DIR = os.path.join("data", "val_samples")
 WANDB_ENCODER_DECODER_PROJECT_NAME = "lofi-encoder"
-LEARNING_RATE = 1e-4
+LEARNING_RATE = 2e-6
 TRAINING_CONFIG = {
     "accelerator": "gpu",
     "devices": 1,
     "precision": "16-mixed",
     "accumulate_grad_batches": 2,
-    "max_steps": 1e10,
     "check_val_every_n_epoch": 2,
     "max_time": "00:24:00:00",
     "enable_checkpointing": True,
-    "gradient_clip_val": 1.5,
+    #"gradient_clip_val": 1.5,
     "limit_train_batches": 30,
     "profiler": "advanced",
     "limit_val_batches": 1.0,
--- Encoder_Decoder_Training/main.py
+++ Encoder_Decoder_Training/main.py
@@ -3,10 +3,12 @@ import datetime
 import lightning as L
 from archisound import ArchiSound
 from torch.utils.data import DataLoader
+from torch import load
 
 from constants import WANDB_ENCODER_DECODER_PROJECT_NAME, TRAINING_CONFIG
 from Data_Processing import pre_processor
 from lightning_torch import LitAudioEncoder
+from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 def check_num_workers(test_dataloader):
@@ -19,11 +21,19 @@ def check_num_workers(test_dataloader):
 
     print(f"Average: {total/20}")
 
-
-if __name__ == "__main__":
+def execute_training_pipeline(pre_process_data = False, 
+                              train_split_prop = 0.99, 
+                              save_top_k_models = 3,
+                              save_on_n_epochs = 5,
+                              model_path = None):
+    """
+    Function for executing entire training pipeline
+    """
+    # Pipeline
     preprocessor = pre_processor.PreProcessor()
-    preprocessor.preprocess(verbose=True)
-    train_set, val_set = preprocessor.split_into_train_val(train_prop=0.7)
+    if pre_process_data:
+        preprocessor.preprocess(verbose=True)
+    train_set, val_set = preprocessor.split_into_train_val(train_prop=train_split_prop)
     train_dataloader = DataLoader(
         train_set,
         batch_size=32,
@@ -38,16 +48,23 @@ if __name__ == "__main__":
         num_workers=6,
         persistent_workers=True,
     )
-
-    autoencoder = ArchiSound.from_pretrained(
-        "dmae1d-ATC32-v3",
-    )
-    lightning_model = LitAudioEncoder(
-        model=autoencoder, project_name=WANDB_ENCODER_DECODER_PROJECT_NAME
-    )
-    trainer = L.Trainer(**TRAINING_CONFIG)
+    if not model_path:
+        autoencoder = ArchiSound.from_pretrained(
+            "dmae1d-ATC32-v3",
+        )
+    else:
+        autoencoder = load(model_path)
+    lightning_model = LitAudioEncoder(model=autoencoder, project_name=WANDB_ENCODER_DECODER_PROJECT_NAME)
+    check_point_callback = ModelCheckpoint(dirpath = 'Model_Directory',
+                                           save_top_k=save_top_k_models,
+                                           every_n_epochs= save_on_n_epochs,
+                                           monitor='val_loss')
+    trainer = L.Trainer(**TRAINING_CONFIG,callbacks = [check_point_callback])
     trainer.fit(
         lightning_model,
         train_dataloaders=train_dataloader,
         val_dataloaders=test_dataloader,
     )
+
+if __name__ == "__main__":
+    execute_training_pipeline(pre_process_data = True)
