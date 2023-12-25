7f7e59e0d7ff5820c715ec3751342cf99f8148c5
ChristianMitrache <camitrac@uwaterloo.ca>
2023-12-25 15:26:54 -0500

    Comitting bug fixes

--- Encoder_Decoder_Training/main.py
+++ Encoder_Decoder_Training/main.py
@@ -1,14 +1,24 @@
+"""Run this file to train the model"""
 import datetime
+import os
 
 import lightning as L
+from pytorch_lightning.loggers import WandbLogger
 from archisound import ArchiSound
-from torch.utils.data import DataLoader
+from lightning.pytorch.callbacks import ModelCheckpoint, EarlyStopping
 from torch import load
+from torch.utils.data import DataLoader
 
-from constants import WANDB_ENCODER_DECODER_PROJECT_NAME, TRAINING_CONFIG
+import wandb
+from constants import (
+    BATCH_SIZE,
+    MODEL_DIRECTORY,
+    TRAINING_CONFIG,
+    WANDB_ENCODER_DECODER_PROJECT_NAME,
+    LEARNING_RATE,
+)
 from Data_Processing import pre_processor
 from lightning_torch import LitAudioEncoder
-from pytorch_lightning.callbacks import ModelCheckpoint
 
 
 def check_num_workers(test_dataloader):
@@ -21,11 +31,14 @@ def check_num_workers(test_dataloader):
 
     print(f"Average: {total/20}")
 
-def execute_training_pipeline(pre_process_data = False, 
-                              train_split_prop = 0.99, 
-                              save_top_k_models = 3,
-                              save_on_n_epochs = 5,
-                              model_path = None):
+
+def execute_training_pipeline(
+    pre_process_data=False,
+    train_split_prop=0.99,
+    save_top_k_models=3,
+    save_on_n_epochs=5,
+    model_path=None,
+):
     """
     Function for executing entire training pipeline
     """
@@ -36,16 +49,16 @@ def execute_training_pipeline(pre_process_data = False,
     train_set, val_set = preprocessor.split_into_train_val(train_prop=train_split_prop)
     train_dataloader = DataLoader(
         train_set,
-        batch_size=32,
+        batch_size=BATCH_SIZE,
         shuffle=True,
-        num_workers=6,
+        num_workers=5,
         persistent_workers=True,
     )
     test_dataloader = DataLoader(
         val_set,
-        batch_size=32,
-        shuffle=True,
-        num_workers=6,
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        num_workers=5,
         persistent_workers=True,
     )
     if not model_path:
@@ -54,17 +67,47 @@ def execute_training_pipeline(pre_process_data = False,
         )
     else:
         autoencoder = load(model_path)
-    lightning_model = LitAudioEncoder(model=autoencoder, project_name=WANDB_ENCODER_DECODER_PROJECT_NAME)
-    check_point_callback = ModelCheckpoint(dirpath = 'Model_Directory',
-                                           save_top_k=save_top_k_models,
-                                           every_n_epochs= save_on_n_epochs,
-                                           monitor='val_loss')
-    trainer = L.Trainer(**TRAINING_CONFIG,callbacks = [check_point_callback])
+
+    config_dict = TRAINING_CONFIG.copy()
+    config_dict["Learning_Rate"] = LEARNING_RATE
+    # Initializing Wandb
+    wandb.login(key=os.getenv("WANDB_API_KEY"))
+    wandblogger = WandbLogger(
+        # set the wandb project where this run will be logged
+        project=WANDB_ENCODER_DECODER_PROJECT_NAME,
+        name=f"{WANDB_ENCODER_DECODER_PROJECT_NAME}-run-test",
+        # track hyperparameters and run metadata
+        config=config_dict,
+    )
+    lightning_model = LitAudioEncoder(model=autoencoder)
+    config_dict["num_val_sample_steps"] = lightning_model.val_sample_steps
+
+    if not os.path.exists(MODEL_DIRECTORY):
+        os.makedirs(MODEL_DIRECTORY)
+
+    check_point_callback = ModelCheckpoint(
+        dirpath=MODEL_DIRECTORY,
+        save_top_k=save_top_k_models,
+        every_n_epochs=save_on_n_epochs,
+        monitor="val_loss",
+    )
+
+    early_stop_callback = EarlyStopping(
+        monitor="val_loss", min_delta=0.01, patience=8, verbose=False, mode="min"
+    )
+
+    trainer = L.Trainer(
+        **TRAINING_CONFIG,
+        callbacks=[check_point_callback, early_stop_callback],
+        logger=wandblogger,
+    )
     trainer.fit(
         lightning_model,
         train_dataloaders=train_dataloader,
         val_dataloaders=test_dataloader,
     )
+    wandb.finish()
+
 
 if __name__ == "__main__":
-    execute_training_pipeline(pre_process_data = True)
+    execute_training_pipeline(pre_process_data=True)
